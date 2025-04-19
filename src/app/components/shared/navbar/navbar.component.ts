6183b331c5a6e6733eba2f5de2b78901683bce01
Luciano Figueroa <lumusitech@gmail.com>
2025-04-19 14:33:11 -0300

    feat: add style for active anchor using router link active with options exact path

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'shared-navbar',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.component.html',
   styles: ``,
 })
