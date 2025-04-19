e1fd2723821a73423ab58f3f344bee6d60508252
Luciano Figueroa <lumusitech@gmail.com>
2025-04-19 17:03:36 -0300

    feat: add if control flow

--- src/app/pages/dragonball-page/dragonball-page.component.ts
+++ src/app/pages/dragonball-page/dragonball-page.component.ts
@@ -16,7 +16,8 @@ export default class DragonballPageComponent {
   characters = signal<Character[]>([
     { id: 1, name: 'Goku', power: 10000 },
     { id: 2, name: 'Vegeta', power: 9500 },
-    { id: 3, name: 'Trunks', power: 9000 },
-    { id: 4, name: 'Krilin', power: 8000 },
+    { id: 3, name: 'Yamcha', power: 900 },
+    { id: 4, name: 'Trunks', power: 9000 },
+    { id: 5, name: 'Krilin', power: 500 },
   ]);
 }
