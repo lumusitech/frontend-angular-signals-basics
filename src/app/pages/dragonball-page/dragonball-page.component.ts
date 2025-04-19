da75d1823c593477cfbd927ecd3867cf2a04f02e
Luciano Figueroa <lumusitech@gmail.com>
2025-04-19 17:39:32 -0300

    feat: add create hero with inputs

--- src/app/pages/dragonball-page/dragonball-page.component.ts
+++ src/app/pages/dragonball-page/dragonball-page.component.ts
@@ -13,6 +13,9 @@ interface Character {
   styles: ``,
 })
 export default class DragonballPageComponent {
+  name = signal<string>('Gohan');
+  power = signal<number>(1100);
+
   characters = signal<Character[]>([
     { id: 1, name: 'Goku', power: 10000 },
     { id: 2, name: 'Vegeta', power: 9500 },
@@ -20,4 +23,23 @@ export default class DragonballPageComponent {
     { id: 4, name: 'Trunks', power: 9000 },
     { id: 5, name: 'Krilin', power: 500 },
   ]);
+
+  addCharacter() {
+    if (!this.name() || !this.power() || this.power() < 0) return;
+
+    const newCharacter: Character = {
+      id: this.characters().length + 1,
+      name: this.name(),
+      power: this.power(),
+    };
+
+    this.characters.update((list) => [...list, newCharacter]);
+
+    this.resetFields();
+  }
+
+  resetFields() {
+    this.name.set('');
+    this.power.set(0);
+  }
 }
